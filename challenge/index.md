a4c160b4ad3c22486e05eaeca3abd2d0ef214524
Ben McShane <benjaminmartinmc@gmail.com>
2022-02-09 20:20:58 -0700

    Complete Challenge

---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+# Favorite Books
+Ender's Game
+The Lightning Thief
+It
+Chicken Soup for the Soul
+Lord of the Rings
+
+
+
+Rachel and I both drove because it did not specify that we were to use Driver/Navigator technique (or if it did, we did not see it)
+I had a little difficulty remembering the proper annotation method. I had to look that up.
---
+++ challenge/java.js
@@ -0,0 +1,13 @@
+// named document java because it was in java, and I hadn't gotten that far.
+//variable libraryName is declared and assigned to the String of "Greenwood Library"
+var libraryName = "Greenwood Library";
+//variable title is declared and assigned to the String of "The Lightning Thief"
+var title = "The Lightning Thief";
+var author = "Rick Riordan";
+//variable numberOfPages is declared and assigned to the Number of "500"
+var numberOfPages = 500;
+//variable checkedOut is declared and assigned to the Boolean of "True"
+var checkedOut = true;
+var publishDate = 2001;
+
+var patrons = ["Fred","John", "Abby", "Ricky", "Louie"];
